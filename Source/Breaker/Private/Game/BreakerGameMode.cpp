936c2d6d20cd940e491a27570cee0bf3d4e2cc23
HALLANGMALLANG\MalLangMalLang <kimsn3464@gmail.com>
2026-06-07 21:27:00 +0900

    [ADD] GameInstanceSubsystem - 오브젝트풀 적용

--- Source/Breaker/Private/Game/BreakerGameMode.cpp
+++ Source/Breaker/Private/Game/BreakerGameMode.cpp
@@ -2,4 +2,15 @@
 
 
 #include "Game/BreakerGameMode.h"
+#include "Subsystem/ObjectPool/ObjectPoolSubsystem.h"
 
+void ABreakerGameMode::BeginPlay()
+{
+	if (UObjectPoolSubsystem* PoolSubsystem = GetWorld()->GetGameInstance()->GetSubsystem<UObjectPoolSubsystem>())
+	{
+		if (!PooledObjectData.IsEmpty())
+		{
+			PoolSubsystem->SetupPools(PooledObjectData);
+		}
+	}
+}
--- Source/Breaker/Public/Game/BreakerGameMode.h
+++ Source/Breaker/Public/Game/BreakerGameMode.h
@@ -4,14 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameMode.h"
+#include "Subsystem/ObjectPool/PoolTypes.h"
 #include "BreakerGameMode.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class BREAKER_API ABreakerGameMode : public AGameMode
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<FName, FPooledObjectData> PooledObjectData;
+
+	virtual void BeginPlay() override;
 };
